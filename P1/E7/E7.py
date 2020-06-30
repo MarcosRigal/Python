8e386f31140406ad6b20a383959e03ca3414c9e5
MarcosRigal <riveragavilanmarcos@gmail.com>
2020-06-30 12:35:47 +0200

    E7, E8, E9

---
+++ P1/E7/E7.py
@@ -0,0 +1,4 @@
+NP = float(input("Introduzca la nota de prácticas: "))
+NPC = float(input("Introduzca la nota del parcial: "))
+NF = float(input("Introduzca la nota del examen final: "))
+print("La nota final es: ", ((NP*0.25)+(NPC*0.25)+(NF*0.5)))
---
+++ P1/E8/E8.py
@@ -0,0 +1,9 @@
+A = float(input("Introduce el primer numero: "))
+B = float(input("Introduce el segundo numero: "))
+C = float(input("Introduce el tercero numero: "))
+if A>B and A>C:
+    print("El mayor es: ", A)
+elif B>A and B>C:
+    print("El mayor es: ", B)
+elif C>A and C>B:
+    print("El mayor es: ", C)
